942ab92e5af8decfc8f3df692f36f4b04b0db847
SHANTO <rabbymd55555@gmail.com>
2025-02-16 19:06:23 +0600

    task-2.js file

---
+++ task-2.js
@@ -0,0 +1,23 @@
+// Task-2:
+// You are given an array of numbers. Count how many times the a number is repeated in the array.
+// sample-input: numbers = [5,6,11,12,98, 5]
+// find: 5
+// output: 2
+// sample-input:
+// numbers = [5,6,11,12,98, 5]
+// find: 25
+// output: 0
+
+function findNumber(numbers, find) {
+  let count = 0;
+  for (let i of numbers) {
+    if (i === find) {
+      count++;
+    }
+  }
+  return count;
+}
+const numbers = [5, 6, 11, 12, 98, 5];
+const find = 5;
+const result = findNumber(numbers, find);
+console.log(result);
