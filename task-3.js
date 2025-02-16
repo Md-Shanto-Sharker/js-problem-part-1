430ec74b6dda8284040a7da03329ccf005bd435e
SHANTO <rabbymd55555@gmail.com>
2025-02-16 19:30:41 +0600

    task-2.js file

--- task-3.js
+++ task-3.js
@@ -1,2 +1,18 @@
 // Task-3:
 // Write a function to count the number of vowels in a string.
+
+function vowels(vowels) {
+  let count = 0;
+  for (let i of vowels) {
+    if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u") {
+      count++
+    }
+  }
+  return count
+}
+const string = "education";
+const result = vowels(string);
+console.log(result);
+
+
+
