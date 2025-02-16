9e19f208850c4e1da68f94fd7e882c4a27760fe7
SHANTO <rabbymd55555@gmail.com>
2025-02-16 18:55:05 +0600

    task-1.js file

---
+++ task-1.js
@@ -0,0 +1,9 @@
+// Task-1:
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function fahrenheit(Celsius){
+    const fahrenheit = Celsius * 1.8 + 32;
+    return fahrenheit
+}
+const result = fahrenheit(50);
+console.log(result ,"F");
