cde89b6b490f38473569d77771e67c2c31d86082
SHANTO <rabbymd55555@gmail.com>
2025-02-16 18:20:14 +0600

    swap.js file

---
+++ swap.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 2;
+console.log(a, b);
+const temp = a;
+a = b;
+b = temp;
+console.log(a,b);
+
+// another rules to sawap ..............
+let x= 1;
+let y = 2;
+console.log(x,y);
+[x,y]=[y,x];
+console.log(x,y);
